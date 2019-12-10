5787333e6f7ebc15edef8f62b18dd09487e5a9f8
moritzwa <wallawitsch@gmail.com>
2019-12-10 17:55:49 +0100

    authentification, login and registration

--- client/src/App.js
+++ client/src/App.js
@@ -3,26 +3,38 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom"
 import Navbar from "./components/layout/Navbar"
 import Home from "./components/Pages/Home"
 import About from "./components/Pages/About"
+import Register from "./components/auth/Register"
+import Login from "./components/auth/Login"
 
 import ContactState from "./context/contact/ContactState.js"
+import AuthState from "./context/auth/AuthState.js"
+import AlertState from "./context/alert/AlertState.js"
+import Alerts from "./components/layout/Alerts"
 
 import "./App.css"
 
 const App = () => {
   return (
-    <ContactState>
-      <Router>
-        <Fragment className="App">
-          <Navbar />
-          <div className="container">
-            <Switch>
-              <Route exact path="/" component={Home} />
-              <Route exact path="/about" component={About} />
-            </Switch>
-          </div>
-        </Fragment>
-      </Router>
-    </ContactState>
+    <AlertState>
+      <AuthState>
+        <ContactState>
+          <Router>
+            <Fragment className="App">
+              <Navbar />
+              <div className="container">
+                <Alerts />
+                <Switch>
+                  <Route exact path="/" component={Home} />
+                  <Route exact path="/about" component={About} />
+                  <Route exact path="/register" component={Register} />
+                  <Route exact path="/login" component={Login} />
+                </Switch>
+              </div>
+            </Fragment>
+          </Router>
+        </ContactState>
+      </AuthState>
+    </AlertState>
   )
 }
 
